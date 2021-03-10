f1a1771f7f684fa671a6e791c360b8b6d687836d
STASYA00 <0.0STASYA@GMAIL.COM>
2021-03-10 15:51:55 +0100

    Add image generation script

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import argparse
 import cv2
 import functools
 import json
@@ -6,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from PIL import Image
+import textwrap
 
 import torch
 import torch.backends.cudnn as cudnn
@@ -22,22 +24,56 @@ from config import *
 from dataset import TestUrbanClassDataset
 from utils import *
 
+################################################################################
 
-gen = UNet(input_dim, real_dim).to(device)
-gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
-disc = PatchDiscriminator(input_dim + real_dim).to(device)
-disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
 
-loaded_state = torch.load(model_path)
-gen.load_state_dict(loaded_state["gen"])
-gen_opt.load_state_dict(loaded_state["gen_opt"])
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+		description=textwrap.dedent('''\
+		USAGE: python generate.py images model.pth
 
-test_dataset = TestUrbanClassDataset(img_path, 'a2b')
-test_dataloader = DataLoader(test_dataset, batch_size=1,
-                        shuffle=False, num_workers=0)
+		------------------------------------------------------------------------
 
-for i, batch in enumerate(test_dataloader):
-	with torch.no_grad():
-		test_image = batch[0].to(device)
-		output = gen( test_image )
-		save_image(output, '{}/{}'.format(img_save, batch[2][0][:-4]))
+		This is an algorithm that generates images with a pretrained pix2pix 
+		weights.
+
+		------------------------------------------------------------------------
+
+		'''), epilog=textwrap.dedent('''\
+		The algorithm will be updated with the changes made in the strategy.
+		'''))
+
+	parser.add_argument('images', type=str, help='path to the image dir',
+						default=None)
+	parser.add_argument('model', type=str, help='path to the model weights',
+						default=None)
+
+	############################################################################
+
+	args = parser.parse_args()
+
+	FOLDER = args.images
+
+	MODEL = args.model
+
+	############################################################################
+
+	gen = UNet(input_dim, real_dim).to(device)
+	gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
+	disc = PatchDiscriminator(input_dim + real_dim).to(device)
+	disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
+
+	loaded_state = torch.load(MODEL)
+	gen.load_state_dict(loaded_state["gen"])
+	gen_opt.load_state_dict(loaded_state["gen_opt"])
+
+	test_dataset = TestUrbanClassDataset(FOLDER, 'a2b')
+	test_dataloader = DataLoader(test_dataset, batch_size=1,
+	                             shuffle=False, num_workers=0)
+
+	for i, batch in enumerate(test_dataloader):
+		with torch.no_grad():
+			test_image = batch[0].to(device)
+			output = gen(test_image)
+			save_image(output, '{}/{}'.format(img_save, batch[2][0][:-4]))
